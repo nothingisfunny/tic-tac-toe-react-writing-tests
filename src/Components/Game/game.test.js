a5a55a551095e602439b3ae7f8993702425589f1
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-03-19 16:38:10 -0700

    add game status test to game to check that 'Next Player' info changes

--- src/Components/Game/game.test.js
+++ src/Components/Game/game.test.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import Game from './game'
-import {shallow} from 'enzyme'
+import {shallow, mount} from 'enzyme'
 
 it('renders without crashing', () => {
   shallow(<Game />);
 });
+
+it('renders game status correctly', () => {
+  const wrapper = mount(<Game/>)
+  const firstPlayer = wrapper.find('div.game-info').children().first().text()
+  expect(firstPlayer).toEqual('Next player: X')
+
+  const button = wrapper.find('button.square').first()
+  button.simulate('click')
+  const secondPlayer = wrapper.find('div.game-info').children().first().text()
+  expect(secondPlayer).toEqual('Next player: O')
+
+})
