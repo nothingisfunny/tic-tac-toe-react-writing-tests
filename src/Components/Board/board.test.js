b25a0586ab92d93441d5edb7238a5d1a14805299
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-03-18 22:16:24 -0700

    fix Board component render test by passing squares prop to Board

--- src/Components/Board/board.test.js
+++ src/Components/Board/board.test.js
@@ -3,5 +3,6 @@ import Board from './board'
 import {shallow} from 'enzyme'
 
 it('renders without crashing', () => {
-  shallow(<Board />);
+  let squares = Array(9).fill(null)
+  shallow(<Board squares={squares}/>);
 });
