f6e177dd04aceaf7a4b37b14f3a92e135662c2c5
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-03-19 17:18:40 -0700

    add test for winner status

--- src/Components/Game/game.test.js
+++ src/Components/Game/game.test.js
@@ -16,4 +16,18 @@ it('renders game status correctly', () => {
   const secondPlayer = wrapper.find('div.game-info').children().first().text()
   expect(secondPlayer).toEqual('Next player: O')
 
+  //player 2
+  const turn2 = wrapper.find('button.square').at(1)
+  turn2.simulate('click')
+  //player 1
+  const turn3 = wrapper.find('button.square').at(4)
+  turn3.simulate('click')
+  //player 2
+  const turn4 = wrapper.find('button.square').at(5)
+  turn4.simulate('click')
+  //player 1
+  const turn5 = wrapper.find('button.square').at(8)
+  turn5.simulate('click')
+  const winner = wrapper.find('div.game-info').children().first().text()
+  expect(winner).toEqual('Winner: X')
 })
